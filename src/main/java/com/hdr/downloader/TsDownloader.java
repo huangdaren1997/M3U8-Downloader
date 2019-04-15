ffc2ada6c2b5edb62688483606e3924df43b5a62
hdr <huangdaren1997@gmail.com>
2019-04-15 20:15:07 +0800

    把Downloader融入到TsDownloader

--- src/main/java/com/hdr/downloader/TsDownloader.java
+++ src/main/java/com/hdr/downloader/TsDownloader.java
@@ -1,15 +1,23 @@
 package com.hdr.downloader;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpStatus;
 import com.hdr.ts.Ts;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.java.Log;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 
 /**
  * @author Huang Da Ren
@@ -34,15 +42,52 @@ public class TsDownloader {
 		int nThreads = 10;
 		ExecutorService executor = Executors.newFixedThreadPool(nThreads);
 		for (int i = 0; i < nThreads; i++) {
-			executor.submit(new Downloader(tsQueue));
+			executor.submit(new Downloader());
 		}
 		executor.shutdown();
 		while (!executor.isTerminated()) {
 
 		}
 
-
 	}
 
+
+	private class Downloader implements Runnable {
+
+		@Override
+		public void run() {
+
+			while (true) {
+				try {
+					Ts ts = tsQueue.poll(1, TimeUnit.MINUTES);
+					if (ts == null) {
+						log.log(Level.WARNING, "thread {0} ts == null", Thread.currentThread().getName());
+						break;
+					}
+					HttpResponse response = HttpRequest.get(ts.getUrl())
+							.header(ts.getHeaders())
+							.execute();
+
+					if (response.getStatus() == HttpStatus.HTTP_OK) {
+						try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(ts.getSavePath()))) {
+							bos.write(response.bodyBytes());
+							bos.flush();
+							log.log(Level.INFO, "{0}下载成功", ts.getSavePath());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					} else {
+						log.log(Level.INFO, "{0}下载失败", ts.getSavePath());
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+
+	}
 
 }
